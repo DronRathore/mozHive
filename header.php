b2d79d6a134f4b0d27f7caa2f9b9e34cb8ca6677
Dron Rathore <dron.rathore@gmail.com>
2013-08-06 11:06:33 +0530

    No submenus
    @debloper: Submenus Gone..! :p

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-$path = "http://localhost/mozHive";
-$header=<<<HEADER
+$path = "http://geekved.com/hive";
+$header = <<<HEADER
 <body>
 	<div class="mozH-header-wrap">
 		<div class="mozH-head">
@@ -12,40 +12,10 @@ $header=<<<HEADER
 			<div class="mozH-rfloat">
 				<ul class="mozH-noStyle">
 					<li><a href="$path" class="mozH-topL">HOME</a></li>
-					<li>
-					<div class="mozH-flyout-wrap">
-							<div class="mozH-flyout">
-								<div class="mozH-pointer"></div>
-								<div class="mozH-links">
-									<a href="$path/about">ABOUT HIVE INDIA</a>
-									<a href="$path/team">THE TEAM</a>
-								</div>
-							</div>
-					</div>
-					<a href="$path/about" class="mozH-topL">HIVE INDIA</a></li>
-					<li>
-					<div class="mozH-flyout-wrap mozH-flyoutFix">
-						<div class="mozH-flyout">
-							<div class="mozH-pointer"></div>
-							<div class="mozH-links">
-								<a href="$path/popup-events">WORKSHOPS</a>
-							</div>
-							</div>
-					</div>
-<a href="$path/popup-events" class="mozH-topL">POP-UP EVENT</a></li>
-					<li>
-					<div class="mozH-flyout-wrap">
-							<div class="mozH-flyout">
-								<div class="mozH-pointer mozH-fixPointer"></div>
-								<div class="mozH-links">
-									<a href="$path/tools">ALL TOOLS</a>
-									<a href="https://popcorn.webmaker.org/">POPCORN MAKER</a>
-									<a href="http://thimble.webmaker.org/">THIMBLE MAKER</a>
-									<a href="http://thimble.webmaker.org/">X-RAY TOOL</a>
-								</div>
-							</div>
-					</div>
-					<a href="$path/tools" class="mozH-topL">TOOLS</a></li>
+					<li><a href="$path/about" class="mozH-topL">HIVE INDIA</a></li>
+					<li><a href="$path/team" class="mozH-topL">THE TEAM</a></li>
+					<li><a href="$path/popup-events" class="mozH-topL">POP-UP EVENT</a></li>
+					<li><a href="$path/tools" class="mozH-topL">TOOLS</a></li>
 					<li><a href="$path/contact" class="mozH-topL">CONTACT</a></li>
 				</ul>
 			</div>
