48bc2e2d5b28621a96aee7bf3575b4bb363d6178
Soumya Deb <sodeb@redhat.com>
2013-08-07 16:04:15 +0530

    Changed navigation links, merged contact & team to about, updated .htaccess

--- about.php
+++ about.php
@@ -45,12 +45,80 @@
 			<!-- Upto Here -->
 		</div>
 	</div>
+	<div class="mozH-banner-wrap mozH-smallBlock">
+		<div class="mozH-banner">
+			<div class="mozH-bannerTag mozH-fixBanner mozH-fixForOther">
+				<h1>Hive India Team</h1>
+				Join our awesome army!
+			</div>
+			<img src="./team_1.jpg"/>
+		</div>
+	</div>
+	<div class="mozH-content-wrap">
+		<div class="mozH-content">
+			<div class="mozH-heading">
+				Our Troops!
+			</div>
+			<div class="mozH-text">
+				<p>We are not an ordinary team working with team leaders or following the workflows, instead each and every memebr in our community is a leader of his/her field. We work not for profit, neither to raise any charity but we work to help spread the technology and take it to everyone.</p>
+				<p>If you find someone helping people at Hive event jumping from music jam to comic creator with immense energy and coolness then its surely our team member. We help people to build things, let them find the right resources and tools required to excel their creativity</p>
+				<p>Sounds Awesome? Wanna show your awesomess? How about joining us? You can pick any or all fields in which you want to volunteer. Volunteers are awarded cool goodies and much more. To join, follow any of the below:</p>
+				<ul>
+				<li>
+					Request ping back from <a href="./contact"> contact us page</a>.
+				</li>
+				<li>
+					Contact Vinnel <<a href="mailto:vineelreddypindi@gmail.com">vineelreddypindi@gmail.com</a>> or Soumya <<a href="mailto:debloper@gmail.com">debloper@gmail.com</a>>
+				</li>
+				<li>Catch us on irc <a href="irc://irc.mozilla.org/">#mozillahive</a> <i>[awaiting confirmation]</i></li>
+				<li>Tweet us at <a href="https://twitter.com/intent/tweet?original_referer=http://hive.mozillaindia.org/team&text=I want to join hiveIndia&tw_p=tweetbutton&url=http%3A%2F%2Flocalhost%2FmozHive%2Fteam&via=hiveindia">@hiveindia</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="mozH-centerBodyWrap">
+		<div class="mozH-centerBody">
+			<h3>Contact Hive India</h3>
+			<form action="./contact" method="POST">
+				<!-- I know you are laughing here, but hey you can replace table with alternate -->
+				<table class="mozH-contactForm">
+					<tr>
+						<td>
+							Full Name
+						</td>
+						<td>
+							<input type="text" name="name" autofocus placeholder="Your Full Name" aria-label="Your Full Name"/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Email
+						</td>
+						<td>
+							<input type="email" name="email" required validate placeholder="Your Email Address" aria-label="Your Email Address"/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Your Message
+						</td>
+						<td>
+							<textarea name="message" required placeholder="A brief message you want to send us" aria-label="A brief message you want to send us"></textarea>
+						</td>
+					</tr>
+				</table>
+				<center><input type="submit" class="mozH-submitButton" value="Ping us"/></center>
+			</form>
+		</div>
+	</div>
 <?php
 	/*
 		Looking to add partner or change footer?
 		Open footer.php, look for class mozH-partner,
 		add an image wrapped in the link, just as the jaaga one.
 	*/
+	require_once("./social.php");
+	echo $social;
 	require_once("/footer.php");
 	echo $footer;
 ?>
--- header.php
+++ header.php
@@ -6,16 +6,14 @@ $header = <<<HEADER
 		<div class="mozH-head">
 			<div class="mozH-lfloat">
 				<a href="$path"><div class="mozH-logo">
-				hive<label class="text">India</label>
+				hive<label class="text">INDIA</label>
 				</div></a>
 			</div>
 			<div class="mozH-rfloat">
 				<ul class="mozH-noStyle">
-					<li><a href="$path/about" class="mozH-topL">HIVE INDIA</a></li>
-					<li><a href="$path/team" class="mozH-topL">THE TEAM</a></li>
-					<li><a href="$path/popup-events" class="mozH-topL">POP-UP EVENT</a></li>
+					<li><a href="$path/events" class="mozH-topL">EVENTS</a></li>
 					<li><a href="$path/tools" class="mozH-topL">TOOLS</a></li>
-					<li><a href="$path/contact" class="mozH-topL">CONTACT</a></li>
+					<li><a href="$path/about" class="mozH-topL">ABOUT</a></li>
 				</ul>
 			</div>
 		</div>
