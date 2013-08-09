c5a5d1995da44ffae95f32e8b65497bbdac90cb0
Dron Rathore <dron.rathore@gmail.com>
2013-08-09 13:42:10 +0530

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 	<link rel="stylesheet" media="all" href="http://hive.mozillaindia.org/front-end/splash.css"/>
 	<script src="http://hive.mozillaindia.org/front-end/squash.js"></script>
-	<meta http-equiv="description" content="Welcome to Hive India, we promote learning and teaching of web and digital media to the people. We strongly believe in people just like you who are eager to learn the Open and Free Standards of the Web."/>
+	<meta name="description" content="Welcome to Hive India, we promote learning and teaching of web and digital media to the people. We strongly believe in people just like you who are eager to learn the Open and Free Standards of the Web."/>
 	<?php
 		require_once("../og.php");
 		echo $og;
