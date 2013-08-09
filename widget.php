04e973f7f39fb25c35f00233484091951236a934
Dron Rathore <dron.rathore@gmail.com>
2013-08-09 14:10:31 +0530

    Update widget.php

--- widget.php
+++ widget.php
@@ -5,7 +5,7 @@ $widget=<<<WIDGET
 		<div class="mozH-widget">
 			<a href="https://twitter.com/hiveindia"><img src="$path/bird_twitter.png"/></a>
 			<a href="https://facebook.com/hiveindia"><img src="$path/facebook.png"/></a>
-			<a href="http://hive.mozillaindia.org/hive/contact"><img src="$path/chat.png"/></a>
+			<a href="http://hive.mozillaindia.org/contact"><img src="$path/chat.png"/></a>
 		</div>
 	</div>
 
