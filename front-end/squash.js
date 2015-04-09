ed51bc9394a40bc8a9cbc1109475439e1221bba6
Dron Rathore <dron.rathore@gmail.com>
2015-04-09 14:42:53 +0530

    Indentation Fix

--- front-end/squash.js
+++ front-end/squash.js
@@ -20,67 +20,67 @@ function Ajax(url,method,element) {
 		this.method=method?method:(element?element.method:"GET");
 		return this;
 }
-	Ajax.prototype.JSON=function(c){
-		this.object.JSONCallback=c;
-		this.send(this.JSONHandler);
-	};
-	Ajax.prototype.JSONHandler=function(xhr, form) {
-		this.JSONCallback(JSON.parse(this.responseText, this.errorHandler), form);
-	}
-	Ajax.prototype.onError=function(callback){
-		this.object.errorHandler = callback;
-	}
-	Ajax.prototype.data=function(raw_data){
-		/*
-			If it is a Form Element
-		*/
-		if(raw_data.elements){
-			this.d="";
-			this.object.form=raw_data;
-			for(i=0;i<raw_data.elements.length;i++){
-				if(raw_data.elements[i].type!="button"&&raw_data.elements[i].type!="reset"&&raw_data.elements[i].type!="submit")
-				this.d+="&" + raw_data.elements[i].name + "=" + raw_data.elements[i].value;
-			}
-		}else{
-			this.d="";
-			for(elem in raw_data){
-				this.d+="&" + elem + "=" + raw_data[elem];
-			}
-		}
-		return this;
+Ajax.prototype.JSON=function(c){
+	this.object.JSONCallback=c;
+	this.send(this.JSONHandler);
 };
-	Ajax.prototype.send=function(callbacks){
-	if (this.method != "POST")
-		this.object.open(this.method,this.url+(this.d?"?"+this.d:""),true);
-	else
-		this.object.open(this.method,this.url,true);
-		if(this.method=="POST")
-		this.object.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		this.object.callback=callbacks;
-		this.object.onreadystatechange=this.handler;
-	if (this.method != "POST")
-		this.object.send();
-	else	
-		this.object.send(this.d);
-		return this;
-	}
-	Ajax.prototype.getObject = function(){return this.object;}
-	Ajax.prototype.handler=function(){
-		if(this.readyState==4){
-						if(this.status==200){
-							this.callback(this,this.form);
-								
-						} else {
-							if (this.form) 
-							this.form.setAttribute("_ajax",0);
-								if (!this.errorHandler) {
-									console.log("[error] XHR Failed with error code "+this.status+(this.status ==0?", i.e. No Internet Connection":", i.e. Could not reach the server" ));
-							} else {
-								this.errorHandler(this.status, this.form);
-							}
-						}
+Ajax.prototype.JSONHandler=function(xhr, form) {
+	this.JSONCallback(JSON.parse(this.responseText, this.errorHandler), form);
+}
+Ajax.prototype.onError=function(callback){
+	this.object.errorHandler = callback;
+}
+Ajax.prototype.data=function(raw_data){
+	/*
+		If it is a Form Element
+	*/
+	if(raw_data.elements){
+		this.d="";
+		this.object.form=raw_data;
+		for(i=0;i<raw_data.elements.length;i++){
+			if(raw_data.elements[i].type!="button"&&raw_data.elements[i].type!="reset"&&raw_data.elements[i].type!="submit")
+			this.d+="&" + raw_data.elements[i].name + "=" + raw_data.elements[i].value;
+		}
+	} else {
+		this.d="";
+		for(elem in raw_data){
+			this.d+="&" + elem + "=" + raw_data[elem];
 		}
 	}
+	return this;
+};
+Ajax.prototype.send=function(callbacks){
+if (this.method != "POST")
+	this.object.open(this.method,this.url+(this.d?"?"+this.d:""),true);
+else
+	this.object.open(this.method,this.url,true);
+	if(this.method=="POST")
+	this.object.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	this.object.callback=callbacks;
+	this.object.onreadystatechange=this.handler;
+if (this.method != "POST")
+	this.object.send();
+else	
+	this.object.send(this.d);
+	return this;
+}
+Ajax.prototype.getObject = function(){return this.object;}
+Ajax.prototype.handler=function(){
+	if(this.readyState==4){
+		if(this.status==200){
+			this.callback(this,this.form);
+				
+		} else {
+			if (this.form) 
+			this.form.setAttribute("_ajax",0);
+				if (!this.errorHandler) {
+					console.log("[error] XHR Failed with error code "+this.status+(this.status ==0?", i.e. No Internet Connection":", i.e. Could not reach the server" ));
+			} else {
+				this.errorHandler(this.status, this.form);
+			}
+		}
+	}
+}
 JSON={
 	toString:function(data, character, toChar){
 		while (data.indexOf(character)!=-1) {
